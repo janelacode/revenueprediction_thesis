1134e92ce50f80d714a0e6dc207c868799076d73
janelacode <janyl.sagynbaeva@gmail.com>
2023-03-20 20:37:15 +0100

    Add inflation predictions with XGBoost

--- timeseries_boosted.py
+++ timeseries_boosted.py
@@ -8,16 +8,27 @@ from sklearn.metrics import mean_squared_error
 color_pal = sns.color_palette()
 plt.style.use('fivethirtyeight')
 
-def createTimeSeriesDf(filename):
+def createTimeSeriesDf(filename, dateformat='%b_%Y'):
     df=pd.read_excel(filename, parse_dates=True)
     # convert string-like month data to date
-    df["Date"]= pd.to_datetime(df["Date"], format='%b_%Y')
+    df["Date"]= pd.to_datetime(df["Date"], format=dateformat)
     df = df.set_index('Date')
     df['quarter']= df.index.quarter
     df['month']=df.index.month
     df['year']=df.index.year
 
     return df.astype({'month' : 'int32', 'quarter': 'int32'})
+
+def createTimeSeriesDfFull(filename):
+    df=pd.read_excel(filename, parse_dates=True)
+    # convert string-like month data to date
+    df = df.set_index('Date')
+    df['quarter']= df.index.quarter
+    df['month']=df.index.month
+    df['year']=df.index.year
+
+    return df.astype({'month' : 'int32', 'quarter': 'int32'})
+
 
 def createTimeSeriesInflationDf(filename):
     if filename[-3:] == 'csv':
